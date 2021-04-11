cd300e881fc27a41382c4b990e9dd000d62a85f3
Richard Lewis <rlewis@3cloudsolutions.com>
2021-04-11 17:39:33 -0500

    seperate network

--- create-virtualmachines.tf
+++ create-virtualmachines.tf
@@ -1,18 +1,4 @@
 
-resource "azurerm_virtual_network" "subnet" {
-  name                = "${azurerm_resource_group.rg.name}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.subnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
 resource "azurerm_network_interface" "nic" {
   count               = var.node_count
   name                = "${var.vmname}${format("%02d", count.index)}NIC"
