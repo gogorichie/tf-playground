d51d9e6e28748d7aa82585da4e5fce195a053402
Richard Lewis <rlewis@3cloudsolutions.com>
2021-04-11 18:22:51 -0500

    added in storage acct

---
+++ create-storageaccount.tf
@@ -0,0 +1,17 @@
+resource "azurerm_storage_account" "strgacct" {
+  name                     = "${var.resourcegroup}strgacct"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "strgacctdiag" {
+  name               = "strgacctdiag"
+  target_resource_id = azurerm_storage_account.strgacct.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+
+  metric {
+    category = "Transaction"
+  }
+}
