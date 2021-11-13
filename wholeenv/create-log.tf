e6b310b586a39d1e2ab0fafb7e174d8373dde6c8
Richard Lewis <rlewis@3cloudsolutions.com>
2021-11-13 03:47:35 -0600

    update law sku

--- wholeenv/create-log.tf
+++ wholeenv/create-log.tf
@@ -2,7 +2,8 @@ resource "azurerm_log_analytics_workspace" "law" {
   name                = "${azurerm_resource_group.rg.name}-LOG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Free"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
   tags                = local.tags
 
 }
